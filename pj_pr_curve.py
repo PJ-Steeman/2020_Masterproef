c8ab21f0d56946123fb79a257bb46c59f61d179b
Pieter-Jan Steeman <pieterjan.steeman@gmail.com>
2020-05-27 16:27:27 +0200

    aanpassingen nieuwe versie brambox

--- pj_pr_curve.py
+++ pj_pr_curve.py
@@ -1,51 +1,58 @@
-%matplotlib inline
-import brambox.boxes as bbb
+import brambox as bbb
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-annotations = bbb.parse('anno_darknet', 'inria/Test/pos/yolo-labels/', class_label_map={0: 'person'}, image_width=1., image_height=1.)
-annotations = bbb.parse('anno_darknet', 'testing/clean/yolo-labels/', class_label_map={0: 'person'}, image_width=1., image_height=1.)
-patch_simen = bbb.parse('det_coco', 'patch_simen.json', class_label_map={0: 'person'})
-patch_up = bbb.parse('det_coco', 'patch_up.json', class_label_map={0: 'person'})
-clean_results = bbb.parse('det_coco', 'clean_results.json', class_label_map={0: 'person'})
-noise_results = bbb.parse('det_coco', 'noise_results.json', class_label_map={0: 'person'})
-class_results = bbb.parse('det_coco', 'class_shift.json', class_label_map={0: 'person'})
-class_only = bbb.parse('det_coco', 'class_only.json', class_label_map={0: 'person'})
+darknetParser = bbb.io.parser.annotation.DarknetParser(image_dims=[1, 1], class_label_map={0: 'person'})
+COCOParser = bbb.io.parser.detection.CocoParser(class_label_map={0: 'person'})
+
+# annotations = darknetParser.parse('inria/Test/pos/yolo-labels/')
+annotations = bbb.io.load(darknetParser, 'testing/clean/yolo-labels/')
+print(annotations.head())
+patch_results = bbb.io.load(COCOParser, 'patch_results.json')
+print(patch_results.head())
+# patch_up = bbb.io.parse('det_coco', 'patch_up.json', class_label_map={0: 'person'})
+clean_results = bbb.io.load(COCOParser, 'clean_results.json')
+# noise_results = bbb.io.parse('det_coco', 'noise_results.json', class_label_map={0: 'person'})
+# class_results = bbb.io.parse('det_coco', 'class_shift.json', class_label_map={0: 'person'})
+# class_only = bbb.parse('det_coco', 'class_only.json', class_label_map={0: 'person'})
 
 plt.figure()
-teddy = bbb.pr(patch_simen, annotations)['person']
-up = bbb.pr(patch_up, annotations)['person']
-noise = bbb.pr(noise_results, annotations)['person']
-clean = bbb.pr(clean_results, annotations)['person']
-class_shift = bbb.pr(class_results, annotations)['person']
-class_only_pr = bbb.pr(class_only, annotations)['person']
+teddy = bbb.stat.pr(patch_results, annotations)
+print(teddy)
+# up = bbb.stat.pr(patch_up, annotations)['person']
+# noise = bbb.stat.pr(noise_results, annotations)['person']
+clean = bbb.stat.pr(clean_results, annotations)
+# class_shift = bbb.stat.pr(class_results, annotations)['person']
+# class_only_pr = bbb.stat.pr(class_only, annotations)['person']
 
 
 
-#ap = bbb.ap(teddy[0], teddy[1])
-#plt.plot(teddy[1], teddy[0], label=f'Teddy: mAP: {round(ap*100, 2)}%')
+plt.plot(teddy['recall'], teddy['precision'])
+#
+# plt.plot([0, 1.05], [0, 1.05], '--', color='gray')
+#
+# ap = bbb.ap(clean[0], clean[1])
+# plt.plot(clean[1], clean[0], label=f'CLEAN: AP: {round(ap*100, 2)}%')
 
-plt.plot([0, 1.05], [0, 1.05], '--', color='gray')
+# ap = bbb.ap(noise[0], noise[1])
+# plt.plot(noise[1], noise[0], label=f'NOISE: AP: {round(ap*100, 2)}%')
 
-ap = bbb.ap(clean[0], clean[1])
-plt.plot(clean[1], clean[0], label=f'CLEAN: AP: {round(ap*100, 2)}%')
+# ap = bbb.ap(class_shift[0], class_shift[1])
+# plt.plot(class_shift[1], class_shift[0], label=f'OBJ-CLS: AP: {round(ap*100, 2)}%')
 
-ap = bbb.ap(noise[0], noise[1])
-plt.plot(noise[1], noise[0], label=f'NOISE: AP: {round(ap*100, 2)}%')
+# ap = bbb.ap(up[0], up[1])
+# plt.plot(up[1], up[0], label=f'OBJ: AP: {round(ap*100, 2)}%')
 
-ap = bbb.ap(class_shift[0], class_shift[1])
-plt.plot(class_shift[1], class_shift[0], label=f'OBJ-CLS: AP: {round(ap*100, 2)}%')
-
-ap = bbb.ap(up[0], up[1])
-plt.plot(up[1], up[0], label=f'OBJ: AP: {round(ap*100, 2)}%')
-
-ap = bbb.ap(class_only_pr[0], class_only_pr[1])
-plt.plot(class_only_pr[1], class_only_pr[0], label=f'CLS: AP: {round(ap*100, 2)}%')
+# ap = bbb.ap(class_only_pr[0], class_only_pr[1])
+# plt.plot(class_only_pr[1], class_only_pr[0], label=f'CLS: AP: {round(ap*100, 2)}%')
 
 #plt.gcf().suptitle('PR-curve')
-plt.gca().set_ylabel('Precision')
-plt.gca().set_xlabel('Recall')
-plt.gca().set_xlim([0, 1.05])
-plt.gca().set_ylim([0, 1.05])
-plt.gca().legend(loc=4)
-plt.savefig('pr-curve.eps')
+# plt.gca().set_ylabel('Precision')
+# plt.gca().set_xlabel('Recall')
+# plt.gca().set_xlim([0, 1.05])
+# plt.gca().set_ylim([0, 1.05])
+# plt.gca().legend(loc=4)
+# plt.savefig('pr-curve.eps')
 plt.show()
